847e0c16d8271678fd7ca7e5793be4803b14fb13
9Mohamedismail <9Mohamedismail@gmail.com>
2025-06-09 19:45:26 -0400

    added a play button and a playing animation

--- src/components/PlaySong.jsx
+++ src/components/PlaySong.jsx
@@ -1,4 +1,7 @@
 import { useRef, useState, useEffect } from 'react'
+import Lottie from 'lottie-react'
+import equalizerAnim from '../assets/equalizer.json'
+import { FaRegPlayCircle } from 'react-icons/fa'
 import styled from 'styled-components'
 
 const StyledContainter = styled.div`
@@ -6,9 +9,23 @@ const StyledContainter = styled.div`
 	justify-content: space-between;
 	width: 100%;
 `
+const MediaWrapper = styled.div`
+	width: 80px;
+	height: 80px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	svg,
+	canvas,
+	div {
+		width: 100%;
+		height: 100%;
+	}
+`
 
 function PlaySong({ songDuration, songProgress, setSongProgress }) {
-	const [isPlaying, setIsPlaying] = useState(true)
+	const [isPlaying, setIsPlaying] = useState(false)
 
 	const audioRef = useRef(new Audio('/toodeep.mp3'))
 	const timeoutRef = useRef(null)
@@ -51,7 +68,9 @@ function PlaySong({ songDuration, songProgress, setSongProgress }) {
 	return (
 		<StyledContainter>
 			<h1>{`0:${String(Math.floor(songProgress)).padStart(2, '0')}`}</h1>
-			<button onClick={handleAudio}>Play Song</button>
+			<MediaWrapper>
+				{isPlaying ? <Lottie animationData={equalizerAnim} loop autoplay /> : <FaRegPlayCircle onClick={handleAudio} />}
+			</MediaWrapper>
 			<h1> 0:16</h1>
 		</StyledContainter>
 	)
